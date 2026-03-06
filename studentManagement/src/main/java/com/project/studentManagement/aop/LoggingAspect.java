1e6b7b92b13713e5c1bd2e29e6c09b96728f3d18
Dharunjrda <dharunjrda2004@gmail.com>
2026-03-06 14:11:17 +0530

    Implemented Aspect Oriented Programming.

---
+++ studentManagement/src/main/java/com/project/studentManagement/aop/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.project.studentManagement.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+	
+	@Before("execution(* com.project.studentManagement.service.*.*(..))")
+	public void logBefore(JoinPoint jp) {
+		System.out.println("Method Started: "+jp.getSignature().getName());
+	}
+	
+	@After("execution(* com.project.studentManagement.service.*.*(..))")
+	public void logAfter(JoinPoint jp) {
+		System.out.println("Method Ended: "+jp.getSignature().getName());
+	}
+	
+	@Around("execution(* com.project.studentManagement.service.*.(..))")
+	public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("Before Method: "+pjp.getSignature());
+		Object result = pjp.proceed();
+		System.out.println("After Method: "+pjp.getSignature());
+		return result;
+	}
+
+}
