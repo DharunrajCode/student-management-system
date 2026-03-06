f0c10f1f284dad84c185f75efc6d407e7ed969f7
Dharunjrda <dharunjrda2004@gmail.com>
2026-03-06 14:27:05 +0530

    Aop Bugs fixed.

--- studentManagement/src/main/java/com/project/studentManagement/aop/LoggingAspect.java
+++ studentManagement/src/main/java/com/project/studentManagement/aop/LoggingAspect.java
@@ -6,8 +6,10 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggingAspect {
 	
 	@Before("execution(* com.project.studentManagement.service.*.*(..))")
